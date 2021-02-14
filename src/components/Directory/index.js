be5e316f7b3323d2b4501677d27c51963b3cc141
MidwestMister <70042995+timothykemp@users.noreply.github.com>
2021-02-14 12:00:05 -0600

    eliminated unecessary components

--- src/components/Directory/index.js
+++ src/components/Directory/index.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import { Container, Table } from 'react-bootstrap';
-import TableHead from '../TableHead';
-import TableBody from '../TableBody';
+import dateFormat from 'dateformat';
+import style from './style.module.css';
 
 function Directory({ employees }) {
 
     return (
         <Container>
-            <Table striped bordered hover>
-                <TableHead />
-                <TableBody employees={employees} />
+            <Table striped bordered hover className={style.Table}>
+                <thead>
+                    <tr>
+                        <th>Image</th>
+                        <th>Name</th>
+                        <th>Phone</th>
+                        <th>Email</th>
+                        <th>Birthday</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        employees.map((employee) => {
+                            return (
+
+                                <tr key={employee.login.uuid}>
+                                    <td><img src={employee.picture.large} alt={employee.name.first + " " + employee.name.last} /></td>
+                                    <td>{employee.name.first + " " + employee.name.last}</td>
+                                    <td>{employee.phone}</td>
+                                    <td><a href={"mailto:" + employee.email}>{employee.email}</a></td>
+                                    <td>{dateFormat(employee.dob.date, "paddedShortDate")}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
             </Table>
         </Container>
     )
